58df53234c779ed50893ad9923fe0e8cc32c14fc
sdjdll <sdjini4491@163.com>
2026-03-03 17:17:59 +0800

    v1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "sdjini.solution"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
